d11df17cb7e87a928cce19f6b9d0f8112382443b
jtarun4625 <tarunjoshi4625@gmail.com>
2018-03-11 01:27:43 +0530

    Change the port

--- predict-flask.py
+++ predict-flask.py
@@ -42,7 +42,9 @@ def hello_world():
     op_to_restore = graph.get_tensor_by_name("op_to_restore:0")
     
     test = str(sess.run(op_to_restore,feed_dict))
-    return test
+    response = flask.jsonify({'state': test})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8081)
